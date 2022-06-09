88352c1c17b658a7b700cbdf12f3f4e48832e7c5
D-Pats <devinpatel97@gmail.com>
2022-06-09 10:11:59 +0100

    Updated sql-schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -5,8 +5,31 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `customer_id` INT NOT NULL AUTO_INCREMENT,
+    `f_name` VARCHAR(40) NOT NULL,
+    `l_name` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`customer_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`item_id` INT NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NOT NULL,
+    `item_value` DECIMAL(4,2) DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+	`order_id` INT NOT NULL AUTO_INCREMENT,
+    `fk_customer_id` INT NOT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`fk_customer_id`) REFERENCES `ims`.`customers` (`customer_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+	`orders_items_id` INT NOT NULL AUTO_INCREMENT,
+    `fk_order_id` INT NOT NULL,
+    `fk_item_id` INT NOT NULL,
+    PRIMARY KEY (`orders_items_id`),
+    FOREIGN KEY (`fk_order_id`) REFERENCES `ims`.`orders` (`order_id`),
+    FOREIGN KEY (`fk_item_id`) REFERENCES `ims`.`items` (`item_id`)
 );
